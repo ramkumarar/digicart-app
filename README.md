4c9a8133bec47c109cb0baf17df1131eb31d1b2a
ramkumar <ramkumar.a@gmail.com>
2017-08-14 17:34:35 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# digicart-app
+
+### Getting Started
+1. Clone this repository
+2. Run `npm install && npm start`
+3. Visit `0.0.0.0:8080` in your browser
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+const settings = {
+
+  entry: {
+    bundle: [
+      "react-hot-loader/patch",
+      "./src/frontend/index.js"
+    ]
+  },
+  output: {
+    filename: 'assets/js/[name].js',
+    publicPath: "",
+    path: __dirname + '/build'
+  },
+  resolve: {
+    extensions: [".js", ".json", ".css"]
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["es2015", { modules: false }],
+            "stage-2",
+            "react"
+          ],
+          plugins: [
+            "transform-node-env-inline"
+          ],
+          env: {
+            development: {
+              plugins: ["react-hot-loader/babel"]
+            }
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader" // has separate config, see postcss.config.js nearby
+        ]
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve("src/www"),
+    publicPath: "http://localhost:8080/", // full URL is necessary for Hot Module Replacement if additional path will be added.
+    quiet: false,
+    hot: true,
+    historyApiFallback: true,
+    inline: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My Custom template',
+      template: path.join(__dirname, '/src/www/templates/index.ejs'),
+      inject: 'body'
+
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    }),
+  ],
+};
+
+module.exports = settings;
